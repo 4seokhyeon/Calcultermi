3acbe10ced4d0be5f470e8f58364e652924d19cd
4seokhyeon <tktjrgus@naver.com>
2023-07-18 19:25:21 +0900

    터미널 계산기 촤종

--- app/src/main/java/com/first/calcultermi/AdditionOperand.kt
+++ app/src/main/java/com/first/calcultermi/AdditionOperand.kt
@@ -2,7 +2,7 @@ package com.first.calcultermi
 
 class AdditionOperand: AbstractOperation() {
     override fun operate(a: Int, b: Int): Int {
-        return a+b
+        return a + b
     }
 
 }
--- app/src/main/java/com/first/calcultermi/SubtractionOperand.kt
+++ app/src/main/java/com/first/calcultermi/SubtractionOperand.kt
@@ -2,7 +2,7 @@ package com.first.calcultermi
 
 class SubtractionOperand : AbstractOperation(){
     override fun operate(a: Int, b: Int): Int {
-        return a-b
+        return a - b
     }
 
 }
