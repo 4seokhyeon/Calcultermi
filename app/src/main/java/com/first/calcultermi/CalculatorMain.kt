7a4385b0aa72639e37762f7e1f0b14ec116a2090
4seokhyeon <tktjrgus@naver.com>
2023-07-19 14:43:33 +0900

    터미널 계산기 while문 적용

--- app/src/main/java/com/first/calcultermi/CalculatorMain.kt
+++ app/src/main/java/com/first/calcultermi/CalculatorMain.kt
@@ -1,26 +1,41 @@
-package com.first.calcultermi
-
+import com.first.calcultermi.*
 import java.util.Scanner
-
 
 fun main() {
     val scanner = Scanner(System.`in`)
-
-    println("첫번째 숫자를 입력하셈")
-    val a =scanner.nextInt()
-
-    println("두번째 숫자를 입력하셈")
-    val b = scanner.nextInt()
-
-    val addOperation = AdditionOperand()
-    val subtractOperation = SubtractionOperand()
-    val multiplyOperation = MultiplicationOperand()
-    val divideOperation = DivisionOperand()
-
     val calculator = Calculator()
 
-    println("덧셈 결과: ${calculator.calculate(a, b, addOperation)}")
-    println("뺄셈 결과: ${calculator.calculate(a, b, subtractOperation)}")
-    println("곱셈 결과: ${calculator.calculate(a, b, multiplyOperation)}")
-    println("나눗셈 결과: ${calculator.calculate(a, b, divideOperation)}")
+    while (true) {
+        println("첫번째 숫자를 입력하셈 (끝내려면 'q' 입력)")
+        val firstInput = scanner.next()
+
+        if (firstInput == "q") {
+            break
+        }
+
+        val a = firstInput.toInt()
+
+        println("두번째 숫자를 입력하셈")
+        val b = scanner.nextInt()
+
+        println("+,-,*,/ 연산자를 입력하세요 (끝내려면 'q' 입력)")
+        val operator = scanner.next()[0]
+
+        if (operator == 'q') {
+            break
+        }
+
+        val operand = when (operator) {
+            '+' -> AdditionOperand()
+            '-' -> SubtractionOperand()
+            '*' -> MultiplicationOperand()
+            '/' -> DivisionOperand()
+            else -> {
+                println("잘못된 연산자")
+                continue
+            }
+        }
+
+        println("결과: ${calculator.calculate(a, b, operand)}")
+    }
 }
